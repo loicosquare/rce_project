bcab62e2b9e6a09326eafc8b1b2034940e0ab4da
loico square <loicsan07@gmail.com>
2022-02-03 13:18:53 +0100

    adding FrontOffice.

---
+++ FrontOffice/src/app/apps/loginRegister/loginRegister.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginRegister',
+  templateUrl: './loginRegister.component.html',
+  styleUrls: ['./loginRegister.component.css']
+})
+export class LoginRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
